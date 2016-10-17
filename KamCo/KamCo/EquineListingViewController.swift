454ade1d1173f5d5d92d579fecbc610efe0c6402
SathiyaPandian <BBTMacbook@BBT-Macbook.nc.rr.com>
2016-10-16 22:34:24 -0400

    Xcode project
    Project set up created with initial screen and folders

---
+++ KamCo/KamCo/EquineListingViewController.swift
@@ -0,0 +1,9 @@
+//
+//  EquineListingViewController.swift
+//  KamCo
+//
+//  Created by BB&T Macbook on 10/16/16.
+//  Copyright © 2016 Sathiya Macbook. All rights reserved.
+//
+
+import Foundation
